d567ae10c406ab65cfc431afd6813af0351bba67
Martin Tellechea <martin.tellechea@openenglish.com>
2019-01-18 16:35:18 -0300

    B_SalesforceTest renamed

--- src/test/java/LeadForm/B_SalesforceTest.java
+++ src/test/java/LeadForm/B_SalesforceTest.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertTrue;
 
 @Listeners(CustomTestListener.class)
-public class SalesforceTest extends BaseMethods {
+public class B_SalesforceTest extends BaseMethods {
 
 
     @Test
